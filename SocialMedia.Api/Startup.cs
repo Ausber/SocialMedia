bfba4db3fb5676c792ce53d0f2085908ee46e99a
Ausberto Medina <ausbermeca@hotmail.com>
2022-09-28 10:07:50 -0500

    Frist Commit

---
+++ SocialMedia.Api/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Data;
+using SocialMedia.Infraestructure.Repositories;
+using System;
+
+namespace SocialMedia.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //AutoMapper
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddControllers();
+            //Conexion
+            services.AddDbContext<SocialMediaContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SocialMedia")));
+            //Dependencias
+            services.AddTransient<IPostRepository, PostRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
